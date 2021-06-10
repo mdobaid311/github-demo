13d5976be7120e1a710a3d28bb86027f04f5c2db
mdobaid311 <81803774+mdobaid311@users.noreply.github.com>
2021-06-10 14:38:29 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Navigation
+const dropDownItems = document.querySelectorAll(".dropdown-hover");
+
+dropDownItems.forEach((dropdownItem) => {
+  dropdownItem.addEventListener("mouseover", () => {
+    dropdownItem.lastElementChild.style.cssText =
+      "opacity:1;visibility:visible";
+    document.querySelector(".navbar-wrapper").style.background =
+      "linear-gradient(to right,#066399,#2f8fdf,#066399 ";
+  });
+  dropdownItem.addEventListener("mouseout", () => {
+    dropdownItem.lastElementChild.style.cssText = "opacity:0;visibility:hidden";
+    document.querySelector(".navbar-wrapper").style.background = "none";
+  });
+});
+// End of navigation
